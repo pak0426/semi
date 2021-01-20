a3595920a035948cd4ce1cd64672a561fa51cad8
DESKTOP-7H7RA7R\DEV05 <pak4184@naver.com>
2021-01-20 18:01:16 +0900

    210120- cubea

--- semi/src/dao/common/FileDAO.java
+++ semi/src/dao/common/FileDAO.java
@@ -1,22 +1,148 @@
 package dao.common;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
 
 public class FileDAO {
-	public boolean makeFile(int num) {
-		boolean result = false;
+	private final String FILE_DIR = "D:\\FILE_DIR\\";
+	private final String URL	  = "jdbc:oracle:thin:@106.240.249.42:1521:orcl";
+	private final String ID	 	  = "private_hm";
+	private final String PW	  	  = "amho";
+	
+	private Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs 			= null;
+	
+	//JDBC DRIVER INFO
+	public Connection getConnection() throws Exception {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(URL, ID, PW);
+			return conn;
+			
+		}catch(Exception e) {
+			System.out.println("JDBC 연결 실패 : " + e.toString());
+		}
 		
-		String hangle[] = {"가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"};
 		
-		String fileDir = "D:\\filedir\\";
+		return null;
+	}
+	
+	//DAO
+	public int getNextval() throws Exception {
+		int result = 0;
 		
-		String fileName = fileDir + "";
+		try {
+			String query = "SELECT FILE_MAKE_SEQ.NEXTVAL FROM DUAL";
+			
+			conn 	= this.getConnection();
+			pstmt 	= conn.prepareStatement(query);
+			
+			rs 		= pstmt.executeQuery();			
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+			
+			System.out.println("nextval : " + result);
+			
+			rs.close();
+			pstmt.close();
+			conn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("SQL 쿼리 에러");
+		}
+		
+		return result;
+	}
+	
+	public List<FileDTO> getList() {
+//	public List<HashMap<String, String>> getList() {
+		
+		
+//		List<HashMap<String, String>> thisList = new ArrayList<HashMap<String,String>>();
+		
+		FileDTO fileDTO = new FileDTO();		
+		List<FileDTO> thisList = new ArrayList<FileDTO>();
+		
+		try {
+			StringBuffer query = new StringBuffer();
+			
+			query.append("SELECT      										   										  \n");
+			query.append("  DETAIL.WORD_IDX          										     					  \n");
+			query.append("  ,WORD.WORD_TITLE      										     						  \n");
+			query.append("  ,LISTAGG(      										    								  \n");
+			query.append("  (SELECT WORD.WORD_TITLE FROM EPTS.CMTN_WORD WORD WHERE WORD.WORD_IDX = DETAIL.DETAIL_IDX) \n");
+			query.append("  ,','      										     									  \n");
+			query.append("  ) WITHIN GROUP(ORDER BY       												 		      \n");
+			query.append("  (SELECT WORD.WORD_TITLE FROM EPTS.CMTN_WORD WORD WHERE WORD.WORD_IDX = DETAIL.DETAIL_IDX)      										     \n");
+			query.append("  ) AS DETAIL_WORD_TITLE										     						  \n");
+			query.append("FROM EPTS.CMTN_WORD_DETAIL DETAIL      										    		  \n");
+			query.append("LEFT OUTER JOIN EPTS.CMTN_WORD WORD      										   		 	  \n");
+			query.append("ON WORD.WORD_IDX = DETAIL.WORD_IDX      										   			  \n");
+			query.append("GROUP BY DETAIL.WORD_IDX, WORD.WORD_TITLE      									  	      \n");
+			
+			conn 	= this.getConnection();
+			pstmt 	= conn.prepareStatement(query.toString());
+			
+			rs 		= pstmt.executeQuery();			
+			while(rs.next()) {
+				HashMap<String, String> listMap = new HashMap<String, String>();
+				
+//				listMap.put("word_title", rs.getString("word_title"));
+//				listMap.put("detail_word_title", rs.getString("detail_word_title"));
+				
+				fileDTO.setWord_title(rs.getString("word_title"));
+				fileDTO.setDetail_word_title(rs.getString("detail_word_title"));
+				
+//				System.out.println(fileDTO.getWord_title());
+//				System.out.println(fileDTO.getDetail_word_title());
+				
+				thisList.add(fileDTO);
+//				thisList.add(listMap);
+				
+//				System.out.println();
+				
+//				System.out.println("thisList is :" + thisList.get(0));
+//				System.out.println("thisList is :" + thisList.get(1));
+//				System.out.println("thisList is :" + thisList.get(2));
+//				System.out.println("thisList is :" + thisList.get(3));
+//				System.out.println("thisList is :" + thisList.get(4));
+//				System.out.println("thisList is :" + thisList.get(5));
+//				System.out.println("thisList is :" + thisList.get(6));
+//				System.out.println("thisList is :" + thisList.get(7));
+//				thisList.get(0);
+			}
+			
+			for(int i=0; i < thisList.size(); i++) {
+				System.out.println(thisList.get(i).getWord_title());
+			}
+			
+			rs.close();
+			pstmt.close();
+			conn.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("SQL 쿼리 에러");
+		}
+		
+		return thisList;
+	}
+	
+	public void makeFile() {
 		
 		Calendar cal = Calendar.getInstance();
-		
 		
 		String year = Integer.toString(cal.get(Calendar.YEAR));
 		String month = Integer.toString(cal.get(Calendar.MONTH)+1);
@@ -28,39 +154,52 @@ public class FileDAO {
 		
 		String nowtime = year + month + day;
 		
-		File chkFile= new File(fileDir + nowtime + "_" + num + ".txt");
+		String hangle[] = {"가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"};
 		
-		if(chkFile.exists()) {
-			System.out.println("파일존재");
-		}else {
-			System.out.println("파일존재x");
-			fileName = fileDir + nowtime + "_" + num + ".txt";
+		String FILE_NAME = FILE_DIR + "";
+		
+		//디렉토리 체크
+		File chkDir = new File(FILE_DIR);
+		
+		//디렉토리가 존재하지 않는다면
+		if(!chkDir.exists()) {
+			System.out.println("디렉토리 존재X");
+			chkDir.mkdir();
+			System.out.println("디렉토리 생성완료");
+		}
+		
+		try {
+			int count = this.getNextval();
+
+			this.getList();
 			
-			try {
+			File chkFile = new File(FILE_DIR + nowtime + "_" + count + ".txt");
+			//이미 만들어진 파일 존재여부 체크
+			if(!chkFile.exists()) {
+				//파일이 존재하지 않는다면
 				
-				System.out.println(fileName);
+				FILE_NAME = FILE_DIR + nowtime + "_" + count + ".txt";
 				
-				//BufferedWriter와 FileWriter를 조합하여 사용 (속도 상승)
-				BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, true));
+				//ANSI encoding
+				OutputStreamWriter fw = new OutputStreamWriter(new FileOutputStream(FILE_NAME), "UTF8");
 				
 				//파일안에 문자열 쓰기
 				for(int i=0; i < hangle.length; i++) {
 					fw.write(hangle[i] + "\r\n"); 
 				}
-				System.out.println("fileName : " + fileName);
 				
 				fw.flush();
 				
 				//객체 닫기
 				fw.close();
-				result = true;
-				System.out.println("파일 생성 성공");
+				System.out.println("파일 생성 성공" + " -- 생성된 파일 이름 : " + FILE_NAME);
 				
-			}catch(Exception e) {
-				e.printStackTrace();
-				System.out.println("파일 생성 실패");				
+			}else {
+				System.out.println("시퀀스 에러");
 			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-		return result;
 	}
 }
--- semi/src/dao/common/makeTextFile.java
+++ semi/src/dao/common/makeTextFile.java
@@ -5,18 +5,6 @@ public class makeTextFile {
 	public static void main(String[] args) {
 		
 		FileDAO fileDAO = new FileDAO();
-		
-		int num = 0;
-		boolean result = true;
-		
-		result = fileDAO.makeFile(num);
-		
-		while(!result) {
-			num = num + 1;
-			result = fileDAO.makeFile(num);
-			if(result) {
-				break;
-			}
-		}
+		fileDAO.makeFile();
 	}
 }
